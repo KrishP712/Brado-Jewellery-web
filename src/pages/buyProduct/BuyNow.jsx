29870f6f7b289725f03e3644a0b27b39961182e1
KrishP712 <padariyakrish800@gmail.com>
2025-12-26 20:55:07 +0530

    some change

--- src/pages/buyProduct/BuyNow.jsx
+++ src/pages/buyProduct/BuyNow.jsx
@@ -1548,7 +1548,7 @@ const BuyNow = () => {
     };
 
     const result = await dispatch(createOrder(orderPayload));
-    console.log(result);
+    console.log(result,"result");
     const orderId = result?.payload?.order?.orderId;
     if (orderId) {
       dispatch(removeCartDataWithOutToast());
