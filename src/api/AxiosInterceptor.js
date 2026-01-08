2e30300cc7c4ec44f8f8f99f02694d1aeb14b4d5
KrishP712 <padariyakrish800@gmail.com>
2026-01-08 18:11:16 +0530

    some change

--- src/api/AxiosInterceptor.js
+++ src/api/AxiosInterceptor.js
@@ -12,27 +12,30 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const accessToken = localStorage.getItem('usertoken');
-
-    if (accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
+    if (typeof window !== "undefined") {
+      const accessToken = localStorage.getItem('usertoken');
+      if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+      }
     }
-
     return config;
   },
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response.data,
   (error) => {
-    if (error.response?.status === 401) {
+    if (
+      typeof window !== "undefined" &&
+      error.response?.status === 401
+    ) {
       localStorage.removeItem("usertoken");
       store.dispatch(logout());
       window.location.href = "/";
     }
     return Promise.reject(error);
   }
-);
-
-axiosInstance.interceptors.response.use(
-  (response) => response.data,
-  (error) => Promise.reject(error)
 );
 
 export default axiosInstance;
