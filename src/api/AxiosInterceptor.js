cf809a54faa79dcc51c0ab4d50a475c85bef58b0
KrishP712 <padariyakrish800@gmail.com>
2026-01-08 18:12:18 +0530

    some change

--- src/api/AxiosInterceptor.js
+++ src/api/AxiosInterceptor.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import { store } from '../redux/store';
-import { logout } from '../redux/slices/auth';
+// import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -12,30 +11,23 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    if (typeof window !== "undefined") {
-      const accessToken = localStorage.getItem('usertoken');
-      if (accessToken) {
-        config.headers.Authorization = `Bearer ${accessToken}`;
-      }
+    // const accessToken = Cookies.get('accessToken');
+    const accessToken = localStorage.getItem('usertoken');
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
     }
+
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 axiosInstance.interceptors.response.use(
   (response) => response.data,
-  (error) => {
-    if (
-      typeof window !== "undefined" &&
-      error.response?.status === 401
-    ) {
-      localStorage.removeItem("usertoken");
-      store.dispatch(logout());
-      window.location.href = "/";
-    }
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default axiosInstance;
